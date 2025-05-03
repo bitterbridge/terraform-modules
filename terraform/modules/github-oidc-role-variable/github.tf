b939ba1616cae419e91cde0a7181498828f112fd
Nate Douglas <ndouglas@users.noreply.github.com>
2025-05-03 14:44:50 -0400

    Update github-oidc-role-variable.

--- terraform/modules/github-oidc-role-variable/github.tf
+++ terraform/modules/github-oidc-role-variable/github.tf
@@ -1,16 +1,5 @@
-locals {
-  variable_name   = "IAM_ROLE_TO_ASSUME"
-  variable_exists = length(data.github_actions_variables.this.variables) > 0
-}
-
-data "github_actions_variables" "this" {
-  name = local.variable_name
-}
-
 resource "github_actions_variable" "this" {
-  count = local.variable_exists ? 0 : 1
-
   repository    = var.repository_name
-  variable_name = local.variable_name
+  variable_name = "IAM_ROLE_TO_ASSUME"
   value         = var.iam_role_arn
 }
