b37959513b714d4514c80a9bcedac4ae13a5222e
Nate Douglas <ndouglas@users.noreply.github.com>
2025-05-03 16:07:51 -0400

    Fix variable names.

--- terraform/modules/github_app_variables/github.tf
+++ terraform/modules/github_app_variables/github.tf
@@ -5,12 +5,12 @@ locals {
 
 resource "github_actions_variable" "id_secret_path" {
   repository    = var.repository_name
-  variable_name = "GITHUB_APP_ID_SECRET_PATH"
+  variable_name = "APP_ID_SECRET_PATH"
   value         = local.app_id_secret_path
 }
 
 resource "github_actions_variable" "private_key_secret_path" {
   repository    = var.repository_name
-  variable_name = "GITHUB_APP_PRIVATE_KEY_SECRET_PATH"
+  variable_name = "APP_PRIVATE_KEY_SECRET_PATH"
   value         = local.private_key_secret_path
 }
